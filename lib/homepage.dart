8248eb9cdc65523db8c939505c23d48396ca8c4e
Erol SARICA <61289802+erolsarica@users.noreply.github.com>
2024-02-03 21:25:13 +0300

    rate bars added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,1 +1,105 @@
-class HomePage
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class SpeedTestPage extends StatefulWidget {
+  const SpeedTestPage({super.key});
+
+  @override
+  State<SpeedTestPage> createState() => _SpeedTestPageState();
+}
+
+class _SpeedTestPageState extends State<SpeedTestPage> {
+  double showProgress = 0.1;
+  final double _downloadRate = 0.0;
+  final double _uploadRate = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Speed Test', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text(
+              'Progress',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            LinearPercentIndicator(
+              percent: showProgress,
+              lineHeight: 20,
+              backgroundColor: Colors.white,
+              progressColor: Colors.green,
+              barRadius: const Radius.circular(7),
+              center: Text(
+                '${(showProgress * 100).toStringAsFixed(2)}%',
+                style: const TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [                  
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Download Rate',
+                        style: TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Upload Rate',
+                        style: TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${_downloadRate.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${_uploadRate.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            SfRadialGauge(),
+          ],
+        ),
+      ),
+    );
+  }
+}
