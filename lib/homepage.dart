28244d1790171b12f7dc80e4f3576bc9a5ec793e
Erol SARICA <61289802+erolsarica@users.noreply.github.com>
2024-02-07 14:11:11 +0300

    packages added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internet_speed_test/flutter_internet_speed_test.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
@@ -10,23 +11,17 @@ class SpeedTestPage extends StatefulWidget {
 }
 
 class _SpeedTestPageState extends State<SpeedTestPage> {
-  double showProgress = 0.1;
-  final double _downloadRate = 0.0;import 'package:flutter/material.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
-import 'package:syncfusion_flutter_gauges/gauges.dart';
+  double showProgress = 0.0;
+  double _downloadRate = 0.0;
+  double _uploadRate = 0.0;
+  double _displayRate = 0.0;
+  bool isServerInProgress = false;
+  bool speedTestStart = false;
 
-class SpeedTestPage extends StatefulWidget {
-  const SpeedTestPage({super.key});
+  String? _ip;
+  String? _isp;
+  String? _asn;
 
-  @override
-  State<SpeedTestPage> createState() => _SpeedTestPageState();
-}
-
-class _SpeedTestPageState extends State<SpeedTestPage> {
-  double showProgress = 0.1;
-  final double _downloadRate = 0.0;
-  final double _uploadRate = 0.0;
-  final double _displayRate = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,7 +43,7 @@ class _SpeedTestPageState extends State<SpeedTestPage> {
             ),
             const SizedBox(height: 10),
             LinearPercentIndicator(
-              percent: showProgress,
+              percent: showProgress/100.0,
               lineHeight: 20,
               backgroundColor: Colors.white,
               progressColor: Colors.green,
@@ -172,109 +167,115 @@ class _SpeedTestPageState extends State<SpeedTestPage> {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              isServerInProgress ? 'Server is in progress...' : 'IP: ${_ip ?? '__'} | ASN: ${_asn ?? '__'} | ISP: ${_isp ?? '__'}',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                speedTest();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Start Test',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-}
 
-  final double _uploadRate = 0.0;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title: const Text('Speed Test', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const Text(
-              'Progress',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            LinearPercentIndicator(
-              percent: showProgress,
-              lineHeight: 20,
-              backgroundColor: Colors.white,
-              progressColor: Colors.green,
-              barRadius: const Radius.circular(7),
-              center: Text(
-                '${(showProgress * 100).toStringAsFixed(2)}%',
-                style: const TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [                  
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Download Rate',
-                        style: TextStyle(
-                          color: Colors.white,
-                          //fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Upload Rate',
-                        style: TextStyle(
-                          color: Colors.white,
-                          //fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '${_downloadRate.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          //fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        '${_uploadRate.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          //fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 10),
-            SfRadialGauge(
-              axes: [
-                RadialAxis(
-                  ranges: [
-                    GaugeRange(startValue: 0 , endValue: 24,),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+  speedTest() {
+    final speedTest = FlutterInternetSpeedTest();
+    speedTest.startTesting(
+      //true(default)
+      onStarted: () {
+        setState(() {
+          speedTestStart = true;
+        });
+      },
+      onCompleted: (TestResult download, TestResult upload) {
+        setState(() {
+          _downloadRate = download.transferRate;
+          showProgress = 100;
+          _displayRate = _downloadRate;
+          
+        });
+        setState(() {
+          _uploadRate = download.transferRate;
+          showProgress = 100;
+          _displayRate = _uploadRate;
+          speedTestStart = false;
+        });
+      },
+      onProgress: (double percent, TestResult data) {
+        setState(() {
+         if (data.type == TestType.download) {
+            _downloadRate = data.transferRate;
+            showProgress = percent / 100;
+            _displayRate = _downloadRate;
+          } else {
+            _uploadRate = data.transferRate;
+            showProgress = percent / 100;
+            _displayRate = _uploadRate;
+          }
+        });
+      },
+      onError: (String errorMessage, String speedTestError) {
+        print('Error: $errorMessage | $speedTestError');
+      },
+      onDefaultServerSelectionInProgress: () {
+        setState(() {
+          isServerInProgress = true;
+        });
+        //Only when you use useFastApi parameter as true(default)
+      },
+      onDefaultServerSelectionDone: (Client? client) {
+        setState(() {
+          isServerInProgress = false;
+          _ip = client?.ip;
+          _isp = client?.isp;
+          _asn = client?.asn;
+        });
+        //Only when you use useFastApi parameter as true(default)
+      },
+      onDownloadComplete: (TestResult data) {
+        setState(() {
+          _downloadRate = data.transferRate;
+          _displayRate = _downloadRate;        
+        });
+      },
+      onUploadComplete: (TestResult data) {
+        setState(() {
+          _uploadRate = data.transferRate;
+          _displayRate = _uploadRate;
+        });
+      },
+      onCancel: () {
+        // TODO Request cancelled callback
+      },
     );
   }
 }
