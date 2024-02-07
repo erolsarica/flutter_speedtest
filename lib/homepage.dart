22c698c58f526bc560d0e9001fd53517428795a5
Erol SARICA <61289802+erolsarica@users.noreply.github.com>
2024-02-07 10:42:02 +0300

    annotation added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,7 +11,174 @@ class SpeedTestPage extends StatefulWidget {
 
 class _SpeedTestPageState extends State<SpeedTestPage> {
   double showProgress = 0.1;
+  final double _downloadRate = 0.0;import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class SpeedTestPage extends StatefulWidget {
+  const SpeedTestPage({super.key});
+
+  @override
+  State<SpeedTestPage> createState() => _SpeedTestPageState();
+}
+
+class _SpeedTestPageState extends State<SpeedTestPage> {
+  double showProgress = 0.1;
   final double _downloadRate = 0.0;
+  final double _uploadRate = 0.0;
+  final double _displayRate = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: const Text('Speed Test', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text(
+              'Progress',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            LinearPercentIndicator(
+              percent: showProgress,
+              lineHeight: 20,
+              backgroundColor: Colors.white,
+              progressColor: Colors.green,
+              barRadius: const Radius.circular(7),
+              center: Text(
+                '${(showProgress * 100).toStringAsFixed(2)}%',
+                style: const TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Download Rate',
+                        style: TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Upload Rate',
+                        style: TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${_downloadRate.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '${_uploadRate.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          //fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            SfRadialGauge(
+              axes: [
+                RadialAxis(
+                  radiusFactor: 0.85,
+                  minorTicksPerInterval: 1,
+                  tickOffset: 3,
+                  useRangeColorForAxis: true,
+                  interval: 4,
+                  minimum: 0,
+                  maximum: 25,
+                  //showLastLabel: true,
+                  axisLabelStyle: const GaugeTextStyle(
+                    color: Colors.white,
+                  ),
+                  ranges: [
+                    GaugeRange(
+                      color: const Color.fromARGB(255, 47, 247, 54),
+                      startValue: 0,
+                      endValue: 24,
+                      startWidth: 5,
+                      endWidth: 10,
+                    ),
+                  ],
+                  pointers: <GaugePointer>[
+                    NeedlePointer(
+                      value: _displayRate,
+                      enableAnimation: true,
+                      //needleLength: 0.6,
+                      needleColor: Colors.white,
+                      tailStyle: const TailStyle(
+                        color: Colors.white,
+                        borderWidth: 0.02,
+                        borderColor: Colors.red,
+                      ),
+
+                      knobStyle: const KnobStyle(
+                        knobRadius: 0.08,
+                        sizeUnit: GaugeSizeUnit.factor,
+                        borderColor: Colors.white,
+                        color: Colors.white,
+                        borderWidth: 0.035,
+                      ),
+                    ),
+                  ],
+                  annotations: [
+                    GaugeAnnotation(
+                        widget: Container(
+                          child: Text(
+                            _displayRate.toStringAsFixed(2),
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        angle: 90,
+                        positionFactor: 0.7)
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
   final double _uploadRate = 0.0;
   @override
   Widget build(BuildContext context) {
